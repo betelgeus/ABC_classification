99dcaa9ad19493e7c9216ab0d286a6677dd6b2fe
mitya <shehovmitya@gmail.com>
2023-08-02 12:52:20 +0300

    Last commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import shutil
 import torch
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
-from ultralytics import YOLO
+# from ultralytics import YOLO
 from pathlib import Path
 import mapping as mp
 
@@ -26,7 +26,7 @@ app.add_middleware(
 
 UPLOAD_DIR = "data/uploaded_files"
 Path(UPLOAD_DIR).mkdir(parents=True, exist_ok=True)
-model = YOLO('./data/weights/printed_and_written.pt')
+# model = YOLO("./data/weights/printed_and_written.pt")
 
 
 def results_processing(results, letter_index):
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+from main import results_processing
+
+
+def test_results_processing():
+    predict_letter_index = '01_28'
+    letter_index = 0
+    assert results_processing()
