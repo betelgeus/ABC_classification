08421c7d4dc1d50aa609fc0da2fa79797de74dbe
mitya <shehovmitya@gmail.com>
2023-08-04 10:22:41 +0300

    Last commit

--- constants.py
+++ constants.py
@@ -14,9 +14,9 @@ class GlobalConfig:
     """
     def __init__(self) -> None:
         self.MODEL_DIR = "./data/weights/printed_and_written.pt"
-        self.UPLOAD_DIR = os.path.abspath("/data/uploaded_files/")
-        self.TEST_MODEL_DIR = os.path.abspath("./tests/data/weights/printed_and_written.pt")
-        self.TEST_DATA_DIR = os.path.abspath("./tests/data/uploaded_files/")
+        self.UPLOAD_DIR = "./data/uploaded_files/"
+        self.TEST_MODEL_DIR = "./tests/data/weights/printed_and_written.pt"
+        self.TEST_DATA_DIR = "./tests/data/uploaded_files/"
 
 
 global_config = GlobalConfig()
