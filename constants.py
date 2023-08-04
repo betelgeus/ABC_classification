aca48e040e287954665df31ea2082f6d4e4b94f3
mitya <shehovmitya@gmail.com>
2023-08-04 12:27:58 +0300

    Last commit

--- constants.py
+++ constants.py
@@ -22,17 +22,22 @@ class GlobalConfig:
                     падает на загрузке модели, хотя вроде путь правильный
                     No such file or directory:
                     '/home/runner/work/ABC_classification/data/weights/printed_and_written.pt
-                пути UPLOAD_DIR ../, остальные ./
+                путь UPLOAD_DIR ../, остальные ./
                     проходит два теста, падает на последнем
                     FileNotFoundError: [Errno 2]
                     No such file or directory: '/home/runner/work/ABC_classification/data/uploaded_files/1690920920325_1.png'
-                все пути ./
+                путь TEST_UPLOAD_DIR ../, остальные ./
+                    проходит два теста, падает на последнем
+                    FileNotFoundError: [Errno 2]
+                     No such file or directory: '/home/runner/work/ABC_classification/ABC_classification/data/uploaded_files/1690920920325_1.png'
+                 путь TEST_UPLOAD_DIR, TEST_DATA_DIR ../, остальные ./
+
 
         """
         self.MODEL_DIR = "./data/weights/printed_and_written.pt"
         self.UPLOAD_DIR = "./data/uploaded_files/"
         self.TEST_MODEL_DIR = "./tests/data/weights/printed_and_written.pt"
-        self.TEST_DATA_DIR = "./tests/data/uploaded_files/"
+        self.TEST_DATA_DIR = "../tests/data/uploaded_files/"
         self.TEST_UPLOAD_DIR = "../data/uploaded_files/"
 
 
